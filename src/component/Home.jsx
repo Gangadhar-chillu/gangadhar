8f075410cefde1ff061d63f0f28384383b2bfde1
Gangadhar-chillu <gangumasti25@gmail.com>
2023-04-07 18:57:16 +0530

    changed title home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,7 +7,7 @@ function Home(props) {
       <section id="home" className="d-flex align-items-center">
         <div className="container d-flex flex-column align-items-center shadow-lg p-4">
             <h1 className='display-1'>Gangu Yadav</h1>
-            <h3 className='display-6 text-center'>I'm a professional MERN Stack Developer.</h3>
+            <h6 className='display-6 text-center'>I'm a professional MERN, React and  React Native Developer.</h6>
             <NavLink to={`/about`} className="btn btn-outline-info">
                 <i className='bi bi-person-fill'></i> About Me
             </NavLink>
